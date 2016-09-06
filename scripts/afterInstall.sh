e77a5b30c9444f83afbfa84292cddabb0b1788bf
Ryo Arima <a3636tako@gmail.com>
2016-09-06 21:36:23 +0900

    ansible install3

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,6 +2,6 @@
 
 curl -kL https://bootstrap.pypa.io/get-pip.py | python
 /usr/local/bin/pip install ansible
-/usr/local/bin/ansible-playbook -h local ansible/ec2-run.yml
+/usr/local/bin/ansible-playbook -i local ansible/ec2-run.yml
 
 
