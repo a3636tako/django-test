3eae6d61bac96a6bf92826b739b082f4a678dfae
Ryo Arima <a3636tako@gmail.com>
2016-09-06 13:08:19 +0900

    circleci script

---
+++ circleci-deploy-master.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+aws deploy create-deployment \
+  --application-name django-test \
+  --deployment-config-name CodeDeployDefault.OneAtATime \
+  --deployment-group-name django-test  \
+  --description "Circle CI deployment" \
+  --github-location repository="a3636tako/django-test",commitId=${CIRCLE_SHA1}
