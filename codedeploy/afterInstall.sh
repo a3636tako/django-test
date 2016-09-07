3bff90093bc2ac58d9295c7a8847399042b8c436
Ryo Arima <a3636tako@gmail.com>
2016-09-07 16:10:24 +0900

    app spec

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -5,8 +5,10 @@ sudo /usr/local/bin/pip install ansible
 
 if [ "$DEPLOYMENT_GROUP_NAME" = "Production" ]
 then
+  sudo cp -r /tmp/mybook/mybook /var/www/django/mybook-prod
   /usr/local/bin/ansible-playbook -i /tmp/mybook/ansible/hosts -c local /tmp/mybook/ansible/production-run.yml
 else
+  sudo cp -r /tmp/mybook/mybook /var/www/django/mybook
   /usr/local/bin/ansible-playbook -i /tmp/mybook/ansible/hosts -c local /tmp/mybook/ansible/develop-run.yml
 fi
 
