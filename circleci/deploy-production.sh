2de453e1e6cd792e3e110fdd0bc3c25078458644
Ryo Arima <a3636tako@gmail.com>
2016-09-07 16:05:12 +0900

    typo

--- circleci/deploy-production.sh
+++ circleci/deploy-production.sh
@@ -3,7 +3,7 @@
 aws deploy create-deployment \
   --application-name django-test \
   --deployment-config-name CodeDeployDefault.OneAtATime \
-  --deployment-group-name Producion  \
+  --deployment-group-name Production  \
   --region us-west-2  \
   --description "Circle CI deployment" \
   --github-location repository="a3636tako/django-test",commitId=${CIRCLE_SHA1}
